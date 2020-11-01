99a11b562acbb572fa7b35dccb034dcf8f542eab
ramana <ramanarao1997@gmail.com>
2020-10-31 21:11:45 -0500

    Added user routes on backend

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,27 @@ import colors from 'colors'
 import connectToDB from './config/db.js'
 
 import { notFound, errorHandler } from './middleware/errorHandler.js'
-import products from './routes/products.js'
 
+import products from './routes/products.js'
+import user from './routes/user.js'
 
 dotenv.config()
 
 const app = express()
+app.use(express.json())
+
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
 
 connectToDB();
 
-
+// API routes
 app.use('/api/products', products)
+
+app.use('/api/users', user)
+
+
+
 
 
 //  any non-existing route / for 404 errors
@@ -25,4 +33,5 @@ app.use(notFound)
 
 // error handler middleware
 app.use(errorHandler)
+
 
